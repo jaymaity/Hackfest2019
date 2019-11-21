1aa9771b5bb2417ab844835a3573102f675d43cc
cksgh0211 <chanhokim94@gmail.com>
2019-11-21 13:19:51 -0800

    commit the code to fetch/update record

---
+++ database.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+# Connect .db
+def openDB(dbFile):
+    return sqlite3.connect(dbFile)
+
+
+# retrieve one record of the claim number from a table
+def fetchRecord(conn, tableName ,ClaimNumber):
+    c = conn.cursor()
+    number=(ClaimNumber,)
+    c.execute('SELECT * '
+              'FROM '+ tableName + ' '
+              'WHERE CLAIM_NUMBER=?',
+              number)
+
+    return c.fetchone()
+
+# update all records of the claim number from a table
+def updateRecord(conn,tableName, ClaimNumber, Params):
+    c = conn.cursor()
+
+    tableInfo =c.execute('pragma table_info('+tableName+')').fetchall()
+    colsUpdated =''
+    for i in tableInfo:
+        colsUpdated += i[1] + '=?,'
+
+    newValues = Params + (ClaimNumber,)
+
+    c.execute('UPDATE ' + tableName + ' SET ' +
+              colsUpdated.rstrip(',') +
+              'where claim_number =?', newValues)
+    conn.commit()
+
+# Sample Code
+# dbFileName = 'C:\\BigDataTools_Projects\\Hackfest2019\\fraud_detection.db'
+# db = openDB(dbFileName)
+# tableName = 'test'
+# print(fetchRecord(db,tableName,'FFFFCFABBBC665B14234100C4B25451D'))
+# updateRecord(db,tableName,'FFFF9910C8FD3CE4E38469785552F7AE',fetchRecord(db,tableName,'FFFFCFABBBC665B14234100C4B25451D'))
+
+
