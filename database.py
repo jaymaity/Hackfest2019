2d5e391f54894edbff3f17aaf83a24243600059b
cksgh0211 <chanhokim94@gmail.com>
2019-11-21 18:57:54 -0800

    Merge branch 'master' of C:\BigDataTools_Projects\Hackfest2019 with conflicts.

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ import sqlite3
 def openDB(dbFile):
     return sqlite3.connect(dbFile)
 
-
 # retrieve one record of the claim number from a table
 def fetchRecord(conn, tableName ,ClaimNumber):
     conn.row_factory = sqlite3.Row
@@ -20,15 +19,17 @@ def fetchRecord(conn, tableName ,ClaimNumber):
         return None
 
 # update all records of the claim number from a table
-def updateRecord(conn,tableName, ClaimNumber, Params):
+def updateRecord(conn,tableName, ClaimNumber, KeyValues):
     c = conn.cursor()
 
-    tableInfo =c.execute('pragma table_info('+tableName+')').fetchall()
+    # tableInfo =c.execute('pragma table_info('+tableName+')').fetchall()
     colsUpdated =''
-    for i in tableInfo:
-        colsUpdated += i[1] + ','
+    # for i in tableInfo:
+    #     colsUpdated += i[1] + ','
+    for i in KeyValues.keys():
+        colsUpdated += i + '=?,'
 
-    newValues = Params + (ClaimNumber,)
+    newValues = tuple(KeyValues.values()) + (ClaimNumber,)
 
     c.execute('UPDATE ' + tableName + ' SET ' +
               colsUpdated.rstrip(',') +
@@ -38,8 +39,7 @@ def updateRecord(conn,tableName, ClaimNumber, Params):
 # Sample Code
 # dbFileName = 'C:\\BigDataTools_Projects\\Hackfest2019\\fraud_detection.db'
 # db = openDB(dbFileName)
-# tableName = 'test'
-# print(fetchRecord(db,tableName,'FFFFCFABBBC665B14234100C4B25451D'))
-# updateRecord(db,tableName,'FFFF9910C8FD3CE4E38469785552F7AE',fetchRecord(db,tableName,'FFFFCFABBBC665B14234100C4B25451D'))
+# tableName = 'claimsfraudprept'
+# updateRecord(db,tableName,'B7EF1BDD73571FF675637211882C8BC1',fetchRecord(db,tableName,'B7EF1BDD73571FF675637211882C8BC1'))
 
 
--- html/app.js
+++ html/app.js
@@ -29,44 +29,44 @@ var updateStatus= function(data, status){
 var getPredictionDataFromPage = function(){
     var arrayPredict =
     {
-         INCIDENT_TYPE : $("#incident_type").val(),
-         case_status : $("#case_status").val(),
-         LOSS_TYPE : $("#loss_type").val(),
-         STATE : $("#state").val(),
-         LIABILITY : $("#liability").val(),
-         SEGMENT : $("#segment").val(),
-         INVESTIGATED : $("#investigated").val(),
-         INSURED_AGE : $("#insured_age").val(),
-         DEBT : $("#debt").val(),
-         CLAIM_NUMBER : $("#claim_number").val(),
-         INSURED_NAME : $("#insured_name").val(),
-         policy_inception_days : $("#policy_inception_days").val(),
-         days_rem_policy_expiry : $("#days_rem_policy_expiry").val(),
-         loss_date_month : $("#loss_date_month").val(),
-         loss_date_year : $("#loss_date_year").val(),
-         policy_expiration_month : $("#policy_expiration_month").val(),
-         policy_expiration_year : $("#policy_expiration_year").val(),
-         policy_effective_month : $("#policy_effective_month").val(),
-         policy_effective_year : $("#policy_effective_year").val(),
-         LOSS_DATE : $("#loss_date").val(),
-         POLICY_EFFECTIVE : $("#policy_effective").val(),
-         POLICY_EXPIRATION : $("#policy_expiration").val(),
-         potential_fraudster : $("#potential_fraudster").val(),
-         fraud_complete : $("#fraud_complete").val(),
-         fraud_risk_level : $("#fraud_risk_level").val(),
-         fraud_used : $("#fraud_used").val(),
-         location_address : $("#location_address").val(),
-         hit_and_run_ind : $("#hit_and_run_ind").val(),
-         icbc_keymissingstolen : $("#icbc_keymissingstolen").val(),
-         icbc_keyswithvehicle : $("#icbc_keyswithvehicle").val(),
-         icbc_numvehiclekeyset : $("#icbc_numvehiclekeyset").val(),
-         totalloss : $("#totalloss").val(),
-         vehlockind : $("#vehlockind").val(),
-         vehstolenind : $("#vehstolenind").val(),
-         veh_towed_ind : $("#veh_towed_ind").val(),
-         case_status : $("#case_status").val(),
-         NOTES : $("#notes").val(),
-         pred_fraud_percent : $("#pred_fraud_percent").val()
+         INCIDENT_TYPE : ("#incident_type").val(),
+         Y : ("#y").val(),
+         LOSS_TYPE : ("#loss_type").val(),
+         STATE : ("#state").val(),
+         LIABILITY : ("#liability").val(),
+         SEGMENT : ("#segment").val(),
+         INVESTIGATED : ("#investigated").val(),
+         INSURED_AGE : ("#insured_age").val(),
+         DEBT : ("#debt").val(),
+         CLAIM_NUMBER : ("#claim_number").val(),
+         INSURED_NAME : ("#insured_name").val(),
+         policy_inception_days : getDateDiffer(("#loss_date").val(),("#POLICY_EFFECTIVE").val()),
+         days_rem_policy_expiry : getDateDiffer(("#policy_expiration").val(),("#loss_date").val()),
+         loss_date_month : ("#loss_date").val().substr(5,3),
+         loss_date_year : ("#loss_date").val().substr(0,4),
+         policy_expiration_month : ("#policy_expiration").val().substr(5,3),
+         policy_expiration_year : ("#policy_expiration").val().substr(0,4),
+         policy_effective_month : ("#policy_effective").val().substr(5,3),
+         policy_effective_year : ("#policy_effective").val().substr(0,4),
+         LOSS_DATE : ("#loss_date").val(),
+         POLICY_EFFECTIVE : ("#policy_effective").val(),
+         POLICY_EXPIRATION : ("#policy_expiration").val(),
+         potential_fraudster : ("#potential_fraudster").val(),
+         fraud_complete : ("#fraud_complete").val(),
+         fraud_risk_level : ("#fraud_risk_level").val(),
+         fraud_used : ("#fraud_used").val(),
+         location_address : ("#location_address").val(),
+         hit_and_run_ind : ("#hit_and_run_ind").val(),
+         icbc_keymissingstolen : ("#icbc_keymissingstolen").val(),
+         icbc_keyswithvehicle : ("#icbc_keyswithvehicle").val(),
+         icbc_numvehiclekeyset : ("#icbc_numvehiclekeyset").val(),
+         totalloss : ("#totalloss").val(),
+         vehlockind : ("#vehlockind").val(),
+         vehstolenind : ("#vehstolenind").val(),
+         veh_towed_ind : ("#veh_towed_ind").val(),
+         case_status : ("#case_status").val(),
+         notes : ("#notes").val(),
+         pred_fraud_percent : ("#pred_fraud_percent").val()
     }
     console.log(arrayPredict)
     return arrayPredict
@@ -78,7 +78,7 @@ var updateFormData = function(data){
         console.log(jsondata)
         debugger;
         $("#incident_type").val(jsondata.INCIDENT_TYPE)
-        $("#case_status").val(jsondata.case_status)
+        $("#y").val(jsondata.Y)
         $("#loss_type").val(jsondata.LOSS_TYPE)
         $("#state").val(jsondata.STATE)
         $("#liability_status").val(jsondata.LIABILITY)
@@ -113,9 +113,24 @@ var updateFormData = function(data){
         $("#vehstolenind").val(jsondata.vehstolenind)
         $("#veh_towed_ind").val(jsondata.veh_towed_ind)
         $("#case_status").val(jsondata.case_status)
-        $("#notes").val(jsondata.NOTES)
+        $("#notes").val(jsondata.notes)
         $("#pred_fraud_percent").val(jsondata.pred_fraud_percent)
 
 
 }
 
+function convertDateFormat(dateWithMn){
+   var monthsMap = {"Jan" : "01", "Feb" : "02",  "Mar" : "03","Apr" : "04", "May" : "05", "Jun" : "06", "Jul" : "07","Aug" : "08", "Sep" : "09", "Oct" : "10","Nov" : "11","Dec" : "12"}
+   var monthStr = dateWithMn.substr(5,3)
+
+   return dateWithMn.replace(monthStr,monthsMap[monthStr]);
+}
+
+function getDateDiffer(dateString1, dateString2) {
+
+    var date1 = new Date(convertDateFormat(dateString1))
+    var date2 = new Date(convertDateFormat(dateString2))
+
+   return Math.floor((date2 - date1) / (1000*60*60*24));
+
+}
